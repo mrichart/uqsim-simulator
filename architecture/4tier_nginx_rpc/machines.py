d54af2f35c40aa36a24e59c647748b37524c725d
Matias Richart <mrichart@fing.edu.uy>
2024-02-21 15:24:59 +0100

    Add possibility to configure network latency in architecture scripts

--- architecture/4tier_nginx_rpc/machines.py
+++ architecture/4tier_nginx_rpc/machines.py
@@ -1,12 +1,21 @@
-# This file is used to generate the machine.json file for the 4-tier architecture
-#
+import argparse
 import json
 import make_arch as march
 
 NUM_QUE = 30
-CLI_LAT = 0
+
+def parse_args():
+	parser = argparse.ArgumentParser()
+	parser.add_argument("--latency_0_1", type=int, default=0, help="Latency between machine 0 and 1")
+	parser.add_argument("--latency_0_2", type=int, default=0, help="Latency between machine 0 and 2")
+	parser.add_argument("--latency_1_2", type=int, default=0, help="Latency between machine 1 and 2")
+	parser.add_argument("--latency_2_3", type=int, default=0, help="Latency between machine 2 and 3")
+	parser.add_argument("--latency_cli", type=int, default=0, help="Latency between client and machine 0")
+	args = parser.parse_args()
+	return args
 
 def main():
+	args = parse_args()
 	global NUM_QUE
 	core_aff = []
 	for i in range(0, NUM_QUE):
@@ -19,14 +28,14 @@ def main():
 	m2 = march.make_machine(mid = 2, name = "machine_2", cores = 40, netSched = sched)
 	m3 = march.make_machine(mid = 3, name = "machine_3", cores = 40, netSched = sched)
 
-	l1 = march.make_machine_links(0, 1, 0, 100)
-	l2 = march.make_machine_links(0, 2, 0, 100)
-	l3 = march.make_machine_links(1, 2, 0, 100)
-	l4 = march.make_machine_links(2, 3, 0, 100)
+	l1 = march.make_machine_links(0, 1, args.latency_0_1*1000000, 100)
+	l2 = march.make_machine_links(0, 2, args.latency_0_2*1000000, 100)
+	l3 = march.make_machine_links(1, 2, args.latency_1_2*1000000, 100)
+	l4 = march.make_machine_links(2, 3, args.latency_2_3*1000000, 100)
 
 	machines = [m0, m1, m2, m3]
 	links = [l1, l2, l3, l4]
-	machine_json = march.make_machine_json(machines, links, CLI_LAT)
+	machine_json = march.make_machine_json(machines, links, args.latency_cli*1000000)
 
 	with open("./json/machines.json", "w+") as f:
 		json.dump(machine_json, f, indent=2)
